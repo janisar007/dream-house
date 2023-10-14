9710e038a09dd91e1d50d2b327fdc245fe882b3f
Janisar Akhtar <janisarakhtaransari007@gmail.com>
2023-10-14 21:33:53 +0530

    create navbar and backend server

---
+++ api/index.js
@@ -0,0 +1,59 @@
+import express from "express";
+
+const app = express();
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000")
+});
+
+/*
+import React from "react";
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div className="bg-slate-200 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <Link to="/">
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+            <span className="text-slate-500">Dream</span>
+            <span className="text-slate-700">House</span>
+          </h1>
+        </Link>
+
+        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+          <input
+            type="text"
+            placeholder="Search..."
+            // here w-24 sm:w-64 means for small screens width=24 and for big and above screens width=64
+            className="bg-transparent focus:outline-none w-24 sm:w-64"
+          />
+
+          <FaSearch className="text-slate-600" />
+        </form>
+
+        <ul className="flex gap-4">
+          <Link to="/">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              Home
+            </li>
+          </Link>
+
+          <Link to="/about">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              About
+            </li>
+          </Link>
+
+          <Link to="/sign-in">
+          <li className="text-slate-700 hover:underline">Sign in</li>
+          </Link>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
+*/
---
+++ client/src/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Signin from "./pages/Signin";
+import SignOut from "./pages/SignOut";
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sign-in" element={<Signin />} />
+          <Route path="/sign-out" element={<SignOut />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
