465c8e28eb3294ebf5aeb8ed1c8d567a75ece359
Janisar Akhtar <janisarakhtaransari007@gmail.com>
2023-10-19 21:48:02 +0530

    Created the listing litem component and show listings

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -7,5 +7,9 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+
+  //This plug in is added for multiple line clamp. i have added it when i was building the card for listing and want to line clamp for truncate the description of the listing-> This is from github docs for lineclamp using tailwind coz there are no build in class for multiple line truncate->
+  plugins: [
+    require('@tailwindcss/line-clamp'),
+  ],
 }
