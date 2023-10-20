d7161255547940fb031b117313a9b9d9c50c4bc7
Janisar Akhtar <janisarakhtaransari007@gmail.com>
2023-10-20 11:42:45 +0530

    Fixed SignIn

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
-import Signin from "./pages/SignIn";
+import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
@@ -20,7 +20,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
 
-          <Route path="/sign-in" element={<Signin />} />
+          <Route path="/sign-in" element={<SignIn />} />
 
           <Route path="/sign-up" element={<SignUp />} />
 
